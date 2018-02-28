e877c2b883fc3c5080fd8f9a88d36eb586100b6c
Thierry Boulhol <36863865+thierryboulhol@users.noreply.github.com>
2018-02-28 13:47:01 +0100

    Update and rename sql_lancer_lot.sql to sp_lancerLot.sql

--- sql/sp_lancerLot.sql
+++ sql/sp_lancerLot.sql
@@ -8,7 +8,7 @@ go
  * @quantité est la quantité désirée. ce paramètre ne peut être NULL et doit être un entier *
  *      strictement positif.                                                                *
  ********************************************************************************************/
-ALTER PROCEDURE lancerLot
+ALTER PROCEDURE sp_lancerLot
 (
 	@modele TypeIDModele,
 	@quantite int,
@@ -30,26 +30,26 @@ BEGIN
 			SET @codeRetour = 1;
 			SET @messageRetour = 'Le paramètre 2 (@quantite) ne peut être NULL';
 		END
+	ELSE IF @quantite <= 0
+		BEGIN
+			SET @codeRetour = 2;
+			SET @messageRetour = 'La quantité spécifiée est incorrecte';
+		END
 	ELSE
 		BEGIN TRY
 			IF NOT EXISTS
 				(
-					SELECT IdModele
+					SELECT idModele
 					FROM Modele
-					WHERE IdModele = @modele
+					WHERE idModele = @modele
 				)
 				BEGIN
 					SET @codeRetour = 2;
 					SET @messageRetour = 'Le modèle ''' + @modele + ''' n''existe pas.';
 				END
-			ELSE IF @quantite <= 0
-				BEGIN
-					SET @codeRetour = 2;
-					SET @messageRetour = 'La quantité spécifiée est incorrecte';
-				END
 			ELSE
 				BEGIN
-					INSERT INTO Lot (DateFabrication, EtatProduction, EtatControl, IdModele, NbrPieceDemande) OUTPUT INSERTED.IdLot VALUES (GETDATE(), 'Attente', 'Attente', @modele, @quantite);
+					INSERT INTO Lot (dateFabrication, etatProduction, etatControle, idModele, nbrPieceDemande) OUTPUT INSERTED.idLot VALUES (GETDATE(), 'Attente', 'Attente', @modele, @quantite);
 					SET @codeRetour = 0;
 					SET @messageRetour = 'Lot ajouté';
 				END
@@ -65,17 +65,17 @@ GO
 DECLARE @code int;
 DECLARE @msg TypeMessageRetour;
 
-EXECUTE @code = lancerLot 'V8', 10, @msg OUTPUT;
+EXECUTE @code = sp_lancerLot 'V8', 10, @msg OUTPUT;
 PRINT convert(varchar(6), @code) + ' - ' + @msg;
 
-EXECUTE @code = lancerLot 'NOMODEL', 10, @msg OUTPUT;
+EXECUTE @code = sp_lancerLot 'NOMODEL', 10, @msg OUTPUT;
 PRINT convert(varchar(6), @code) + ' - ' + @msg;
 
-EXECUTE @code = lancerLot NULL, 10, @msg OUTPUT;
+EXECUTE @code = sp_lancerLot NULL, 10, @msg OUTPUT;
 PRINT convert(varchar(6), @code) + ' - ' + @msg;
 
-EXECUTE @code = lancerLot 'V8', NULL, @msg OUTPUT;
+EXECUTE @code = sp_lancerLot 'V8', NULL, @msg OUTPUT;
 PRINT convert(varchar(6), @code) + ' - ' + @msg;
 
-EXECUTE @code = lancerLot 'V8', -654, @msg OUTPUT;
+EXECUTE @code = sp_lancerLot 'V8', -654, @msg OUTPUT;
 PRINT convert(varchar(6), @code) + ' - ' + @msg;
