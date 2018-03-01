b09d77ba5b2d31d166ba7f729f18ce0c04414af0
Thierry Boulhol <36863865+thierryboulhol@users.noreply.github.com>
2018-03-01 09:20:08 +0100

    Create sp_annulerLot.sql

---
+++ sql/sp_annulerLot.sql
@@ -0,0 +1,60 @@
+use Pitson;
+go
+
+ALTER PROCEDURE sp_annulerLot
+(
+	@idLot int,
+	@messageRetour TypeMessageRetour OUTPUT
+)
+AS
+BEGIN
+	DECLARE @codeRetour int = -1;
+	SET @messageRetour = 'non implementé';
+
+	IF @idLot IS NULL
+		BEGIN
+			SET @codeRetour = 1;
+			SET @messageRetour = 'Le paramètre 1 (@idLot) ne peut être NULL';
+		END
+	ELSE
+		BEGIN TRY
+			IF NOT EXISTS
+				(
+					SELECT idLot
+					FROM Lot
+					WHERE idLot = @idLot
+				)
+				BEGIN
+					SET @codeRetour = 2;
+					SET @messageRetour = 'Le lot ''' + convert(varchar(10), @idLot) + ''' n''existe pas';
+				END
+			ELSE
+				BEGIN
+					DELETE FROM Lot WHERE idLot = @idLot AND etatProduction = 'Attente';
+					IF @@ROWCOUNT = 0
+						BEGIN
+							SET @codeRetour = 2;
+							SET @messageRetour = 'impossible d''annuler le lot ''' + convert(varchar(10), @idLot) + ''' car sa production a déjà démarré'
+						END
+					ELSE
+						BEGIN
+							SET @codeRetour = 0;
+							SET @messageRetour = 'Le lot '''+ convert(varchar(10), @idLot) +'''a bient été annulé';
+						END
+				END
+		END TRY
+		BEGIN CATCH
+			SET @codeRetour = 3;
+			SET @messageRetour = 'Erreur base de données : ' + ERROR_MESSAGE();
+		END CATCH
+	RETURN @codeRetour;
+END
+GO
+
+
+
+EXECUTE @code = sp_annulerLot 1, @msg OUTPUT;
+PRINT 'AnnulerLot' + convert(varchar(3), @code) + ' - ' + @msg;
+
+EXECUTE @code = sp_annulerLot NULL, @msg OUTPUT;
+PRINT 'AnnulerLot' + convert(varchar(3), @code) + ' - ' + @msg;
