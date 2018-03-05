378311d38d92b48610fa5e377a2abf68b3d0a948
gregoirebrun <36882788+gregoirebrun@users.noreply.github.com>
2018-03-05 13:46:59 +0100

    Update Categorie.java

--- src/entity/Categorie.java
+++ src/entity/Categorie.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package entity;
 
 /**
@@ -11,10 +6,18 @@ package entity;
  */
 public class Categorie
 {
+
     private String nomCategorie; // Nom de la catégorie (Petit/Moyen/Grand/Rebut)
     private double minCategorie; // Taille minimum de la catégorie
     private double maxCategorie; // Taille maximum de la catégorie
 
+    /**
+     * Constructeur de l'objet catégorie
+     *
+     * @param nomCategorie est le nom de la catégorie
+     * @param minCategorie est le minimum de la catégorie
+     * @param maxCategorie est le maximum de la catégorie
+     */
     public Categorie(String nomCategorie, double minCategorie, double maxCategorie)
     {
         this.nomCategorie = nomCategorie;
@@ -22,36 +25,71 @@ public class Categorie
         this.maxCategorie = maxCategorie;
     }
 
+    /**
+     * Constructeur de l'objet catégorie
+     *
+     * @param nomCategorie est le nom de la catégorie
+     */
     public Categorie(String nomCategorie)
     {
         this.nomCategorie = nomCategorie;
     }
 
+    /**
+     * Cette méthode permet de récupérer le nom de la catégorie
+     *
+     * @return retourne le nom de la catégorie
+     */
     public String getNomCategorie()
     {
         return nomCategorie;
     }
 
+    /**
+     * Cette méthode permet de mettre à jour ke nom de la catégorie
+     *
+     * @param nomCategorie est le nom que l'on veut donner à la catégorie
+     */
     public void setNomCategorie(String nomCategorie)
     {
         this.nomCategorie = nomCategorie;
     }
 
+    /**
+     * Cette méthode permet de récupérer le minimum de la catégorie
+     *
+     * @return retourne le minimum de la catégorie
+     */
     public double getMinCategorie()
     {
         return minCategorie;
     }
 
+    /**
+     * Cette méthode permet de mettre à jour le minimum de la catégorie
+     *
+     * @param minCategorie est le minimum que l'on veut donner à la catégorie
+     */
     public void setMinCategorie(double minCategorie)
     {
         this.minCategorie = minCategorie;
     }
 
+    /**
+     * Cette méthode permet de récupérer le maximum de la catégorie
+     *
+     * @return retourne le maximum de la catégorie
+     */
     public double getMaxCategorie()
     {
         return maxCategorie;
     }
 
+    /**
+     * cette méthode permet de mettre à jour le maximum de la catégorie
+     *
+     * @param maxCategorie est le maximim que l'on veut donner à la catégorie
+     */
     public void setMaxCategorie(double maxCategorie)
     {
         this.maxCategorie = maxCategorie;
@@ -61,5 +99,5 @@ public class Categorie
     public String toString()
     {
         return "Categorie : " + "\nNom de la Categorie =" + getNomCategorie() + "\nMinimum de la Categorie = " + getMinCategorie() + "\tMaximum de la Categorie = " + getMaxCategorie();
-    }  
+    }
 }
