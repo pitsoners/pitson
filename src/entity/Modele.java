595d960ce33e554d53dfd13774c969fa2bf53c95
luciendenis <36878151+luciendenis@users.noreply.github.com>
2018-03-05 11:11:06 +0100

    Add files via upload

---
+++ src/entity/Modele.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package entity;
+
+/**
+ *
+ * @author denis
+ */
+public class Modele {
+    private String m_IdModele ;     // nom du modèle
+    private double m_Diametre ;     // diamètre nominal du modèle
+    private boolean m_Obsolete ;    // bouléen qui vaut vrai si le modèle est obsolète
+
+    public Modele(String idModele, double diametre, boolean obsolete)
+    {
+        setIdModele(idModele);
+        setDiametre(diametre);
+        setObsolete(obsolete);
+    }
+    
+    public String getIdModele() {
+        return m_IdModele;
+    }
+
+    public void setIdModele(String idModele) {
+        this.m_IdModele = idModele;
+    }
+
+    public double getDiametre() {
+        return m_Diametre;
+    }
+
+    public void setDiametre(double diametre) {
+        this.m_Diametre = diametre;
+    }
+
+    public boolean isObsolete() {
+        return m_Obsolete;
+    }
+
+    public void setObsolete(boolean obsolete) {
+        this.m_Obsolete = obsolete;
+    }
+    
+}
