73fcdd18855601135058bf2c685e825fa296b36f
gregoirebrun <36882788+gregoirebrun@users.noreply.github.com>
2018-03-09 11:32:40 +0100

    Update Tools.java

--- src/util/Tools.java
+++ src/util/Tools.java
@@ -2,17 +2,18 @@ package util;
 
 public class Tools
 {
-    /**
-     * Cette outil permet de vérifier que la donnée est bien de type mesure 
+        /**
+     * Cette outil permet de vérifier que la donnée est bien de type mesure
+     *
      * @param s est la string que l'on veut vérifier
      * @return retourne vrai si la string est de type mesure
      */
     public static boolean isTypeMesure(String s)
     {
         boolean ok = false; // Booléen retourné par la fonction
-        if ((s != null) && (s.matches("^(\\d{1,2})(,\\d{1,3})?$")))
+        if ((s != null) && (s.matches("^(-?\\d{1,2})(.\\d{1,3})?$")))
         {
-            ok = true; 
+            ok = true;
         }
         return ok;
     }
