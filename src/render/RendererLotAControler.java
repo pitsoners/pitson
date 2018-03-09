ea89055841d431758492e7da827bef25ce353584
Alin Preda <36878403+AlinPreda@users.noreply.github.com>
2018-03-09 10:38:04 +0100

    Add files via upload

---
+++ src/render/RendererLotAControler.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package render;
+
+import entity.Lot;
+import java.awt.Color;
+import java.awt.Component;
+import javax.swing.JLabel;
+import javax.swing.JList;
+import javax.swing.ListCellRenderer;
+
+/**
+ *
+ * @author preda
+ */
+public class RendererLotAControler implements ListCellRenderer<Lot>
+{
+
+    @Override
+    public Component getListCellRendererComponent(JList<? extends Lot> list, Lot lot, int index, boolean isSelected, boolean cellHasFocus)
+    {
+        JLabel lab = new JLabel("Lot n° "+lot.getIdLot()+" - ["+lot.getIdModele()+"] - "+ "Controle en "+ lot.getEtatControle()+ " - "
+                                +lot.getNbrPieceDemande() + " pièces demandées"+" - "+lot.getPiecesControlees() + " pièces controlées");
+        lab.setOpaque(true);
+        if (isSelected)
+        {
+            lab.setBackground(list.getSelectionBackground());
+            lab.setForeground(list.getSelectionForeground());
+        }
+        else
+        {
+            lab.setBackground(Color.WHITE);
+            lab.setForeground(list.getForeground());
+        }
+        if (cellHasFocus)
+        {
+            lab.setBorder(new javax.swing.border.LineBorder(Color.yellow));
+        }
+        
+        return lab;
+    }
+    
+}
